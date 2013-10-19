0264c9c1ceaffd4df6e5223ad59db7f5e1f98acc
Vitor Capela <dodecaphonic@gmail.com>
2013-10-19 03:52:21 -0300

    Remove omniauthable providers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauthable_providers: [:facebook]
+         :omniauthable
 
   # :provider, :uid, :name
 
