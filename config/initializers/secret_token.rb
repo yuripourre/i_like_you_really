a8e20951eba469e38a703e99d858439d4a7ab140
Leonardo Couto <leonardowrc@gmail.com>
2013-10-18 21:59:42 -0300

    Initial project structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ILikeYouReally::Application.config.secret_key_base = '3f2ff8f798a7a38be7fa7fb71be8a0acfb215f5e7e4d2506933164ae54bafbed92ee8c8c54096923e2604808f9b8a67c72c16e3847084296cc421ecef09c3c03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ILikeYouReally::Application.config.session_store :cookie_store, key: '_i_like_you_really_session'
