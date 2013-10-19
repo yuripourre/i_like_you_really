b227b316176bab6f492fab64d22c127dbb1fa7da
Vitor Capela <dodecaphonic@gmail.com>
2013-10-19 14:40:08 -0300

    Deploy settings.yml.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :scm, :git
 # set :format, :pretty
 # set :log_level, :debug
 # set :pty, true
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/settings.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -34,5 +34,4 @@ namespace :deploy do
   end
 
   after :finishing, 'deploy:cleanup'
-
 end
