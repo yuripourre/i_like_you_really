6522566d7d263244a79e042502bf508ed0dfb8dc
thiagocifani <cifani.thiago@gmail.com>
2013-10-19 20:25:22 -0300

    ajaxfy add friends

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -9,6 +9,23 @@ class FacebookController < ApplicationController
     redirect_to root_path
   end
 
+  def toggle_like
+    @relationship = current_user.relationships.where(facebook_user_id: params[:facebook_user_id]).first
+    @relationship.like = !@relationship.like
+    @relationship.save
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def toggle_comment
+    @relationship = current_user.relationships.where(facebook_user_id: params[:facebook_user_id]).first
+    @relationship.comment = !@relationship.comment
+    @relationship.save
+    respond_to do |format|
+      format.js
+    end
+  end
 
 private
   def user_params
