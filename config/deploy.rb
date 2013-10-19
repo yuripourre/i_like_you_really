d6e53a8aa6f06fff22220a9adf477789813b516d
Vitor Capela <dodecaphonic@gmail.com>
2013-10-19 04:00:39 -0300

    Add puma config to deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,6 @@ set :scm, :git
 # set :format, :pretty
 # set :log_level, :debug
 # set :pty, true
-
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
