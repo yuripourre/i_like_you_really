60435ac5eab1449f2b4e9069275d7aec6b120216
Yuri Pourre <yuriamayuri@gmail.com>
2013-10-19 12:04:34 -0300

    Guest Service update

--- app/services/facebook_guest.rb
+++ app/services/facebook_guest.rb
@@ -1,14 +1,14 @@
 class FacebookGuest
 
-  attr_reader :access_token, :test_users
+  attr_reader :test_users, :permissions
 
   def initialize
     @test_users = Koala::Facebook::TestUsers.new(app_id: ENV["FACEBOOK_APP_ID"], secret: ENV["FACEBOOK_APP_SECRET"])
+    @permissions = "read_stream, publish_actions" #see https://developers.facebook.com/docs/reference/login/extended-permissions/
   end
 
   def create_test_user
     has_app_installed = true
-    permissions = "offline_access, read_stream, push_stream"
 
     @test_users.create has_app_installed, permissions
   end
@@ -20,6 +20,16 @@ class FacebookGuest
 
   def delete_test_user test_user
     @test_users.delete test_user
+  end
+
+  def be_friend user1, user2
+    @test_users.befriend(user1, user2)
+  end
+
+  def create_network how_many
+    has_app_installed = true
+
+    @test_users.create_network(how_many, has_app_installed, permissions)
   end
 
 end
