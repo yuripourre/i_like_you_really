96d691d39c2480bcc1dc4cdb9ccd75318212eb86
Yuri Pourre <yuriamayuri@gmail.com>
2013-10-20 20:33:07 -0200

    Guest fix, really

--- app/controllers/facebook_guest_controller.rb
+++ app/controllers/facebook_guest_controller.rb
@@ -1,8 +1,7 @@
 class FacebookGuestController < ApplicationController
 
   def friends
-
-    @user = User.where(:access_token => 'CAAFCzZAYjE0cBAHxBkS1ZBnnGyEHcOx0nBKS1M9fTbKq689dBS4YsdSpQecntFHDy1yEM6jKPOVvwojRgm9pR5Kz02pd9iNiySoHg7ndgFvsqtlU8Net7Jj0dRZCPaJnXXw7i2tdrk0ypfkKOwg6jgSZCV1VlDMBqjMkPBQFaAL3jwskqGZAz').first # Find the user depending on the params
+    @user = User.where(:email => 'dodecaphonic.appstore@gmail.com').first # Find the user depending on the params
     sign_in @user
 
     redirect_to friends_facebook_index_path
