056ea99b599ba6840fa9ce02d5da286d4e1eb964
Vitor Capela <dodecaphonic@gmail.com>
2013-10-19 22:12:54 -0300

    Improves structure; adds a little tiny bit of resilience.

--- app/services/facebook_friends.rb
+++ app/services/facebook_friends.rb
@@ -1,9 +1,10 @@
 class FacebookFriends
 
-  attr_reader :friends, :user
+  attr_reader :facebook, :user
+
   def initialize(user, facebook = Facebook.new(user.access_token))
-    @friends = facebook.friends
-    @user = user
+    @facebook = facebook
+    @user     = user
   end
 
   def save
@@ -26,25 +27,33 @@ class FacebookFriends
     user.relationships.where(facebook_user_id: id).first_or_create
   end
 
+  def facebook_friends
+    @friends ||= @facebook.friends
+  end
+
   def new_relationships
     @bd ||= Set.new(user.friends.pluck("facebook_id"))
-    @friends.select{|f| !@bd.include?(f['id']) }
+    facebook_friends.select{|f| !@bd.include?(f['id']) }
   end
 
   def update_graph
-    new_relationships.each do |f|
-      create_friend(facebook_id: f["id"], name: f["name"])
-      create_relationship(f["id"])
-    end
-    remove_friends
+    mirror_additions
+    mirror_removals
     self
   rescue
     self
   end
 
-  def remove_friends
+  def mirror_additions
+    new_relationships.each do |f|
+      create_friend(facebook_id: f["id"], name: f["name"])
+      create_relationship(f["id"])
+    end
+  end
+
+  def mirror_removals
     bd = Set.new(user.friends.pluck("facebook_id"))
-    ids = bd.select{|id| !@friends.find{ |f| f["id"] == id }}
+    ids = bd.select{|id| !facebook_friends.find{ |f| f["id"] == id }}
     user.relationships.where(facebook_user_id: ids).destroy_all
   end
 end
