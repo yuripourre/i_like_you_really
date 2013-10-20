e2613cb6d62db7a4eeb1988fb1abdbffcb587f9e
Vitor Capela <dodecaphonic@gmail.com>
2013-10-20 21:15:34 -0200

    Change scheduling again.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :environment, ENV["RAILS_ENV"] || "development"
 set :output, error: "log/cron_error.log", standard: "log/cron.log"
 
-every 1.minutes do
-  runner "bundle exec rake interactor:run"
+every 5.minutes do
+  runner "MainWorker.perform_async"
 end
