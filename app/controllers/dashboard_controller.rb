05b7a451f2e1ab77e4af82eae91a0d3f74ffe043
Yuri Pourre <yuriamayuri@gmail.com>
2013-10-20 01:11:31 -0200

    dashboard changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,14 +4,21 @@ class DashboardController < ApplicationController
 
   	@activities = []
     
-
-    #@activity = Activity.new (category: '9', friend_id: '10', value: '100', object_id: '10098808_098765')
-    for i in 0..5
+    for i in 0..11
       activity = Activity.new
       activity.category = 'Like'
+      activity.value = " - "
       activity.friend_id = "#{i}"
+      activity.object_id = "10000845"+"#{i}"
       @activities.push activity
     end
+
+    @activities[3].category = "Comment"
+    @activities[3].value = "Que irado!"
+
+    @activities[5].category = "Comment"
+    @activities[5].value = "Uhul!"
+
   end
 
 end
