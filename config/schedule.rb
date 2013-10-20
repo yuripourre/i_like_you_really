ea83d9cb9fd81cc5ddee3cd21f6b6a97df107199
Vitor Capela <dodecaphonic@gmail.com>
2013-10-20 17:51:18 -0200

    Set schedule.rb's proper env.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,5 @@
-set :environment, "development"
+set :environment, ENV["RAILS_ENV"] || "development"
+set :output, error: "log/cron_error.log", standard: "log/cron.log"
 
 every 1.minutes do
   runner "MainWorker.perform_async"
