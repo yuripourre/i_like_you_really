c41e31ee55b217799bd3d68e17ac045c7eb012a6
Yuri Pourre <yuriamayuri@gmail.com>
2013-10-20 18:24:10 -0200

    Distinct in dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
   def index
     @comment_count = current_user.activities.comments.count
     @like_count    = current_user.activities.likes.count
-    @last_people   = current_user.activities.latest.limit(12).map { |a|
+    @last_people   = current_user.activities.latest.limit(12).distinct.map { |a|
 
       relationship = current_user.relationships.find_by(facebook_user_id: a.friend_id)
       RelationshipDecorator.new(relationship)
